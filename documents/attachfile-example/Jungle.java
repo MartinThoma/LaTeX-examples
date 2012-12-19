a3e4ead5c49ca5ed0d49c11695df9ecb8d8352a8
Martin Thoma <info@martin-thoma.de>
2012-12-19 10:08:25 +0100

    added attachfile example

---
+++ documents/attachfile-example/Jungle.java
@@ -0,0 +1,7 @@
+public class Jungle {
+    public static void main(String[] args) {
+        Animal felix = new Cat();
+        System.out.println(felix.getCatSound());
+        System.out.println(felix.getSound());
+    }
+}
