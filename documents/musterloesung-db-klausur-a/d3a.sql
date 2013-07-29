62f30e1a58e9dda55e52d6b12edbde54b3d2df79
Martin Thoma <info@martin-thoma.de>
2013-07-29 17:22:55 +0200

    Musterloesung existiert bereits

--- documents/musterloesung-db-klausur-a/d3a.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW GoalsPerPlayer AS (
-    SELECT player_id, name, team, SUM(goals) AS sum_goals
-        FROM Player
-        JOIN Participation ON Participation.player_id = Player.player_id
-        GROUP BY player_id 
-)
