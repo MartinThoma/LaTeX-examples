097d3604233e058171d46c67539ffd18bf596e54
Stefan Koch <taikano@lavabit.com>
2013-07-30 11:18:13 +0200

    MAX in WHERE not possible
    You cannot use MAX() in a WHERE clause, instead use a subquery.
    
    The ORDER BY is also unneeded, because we only select the maximum. So what to order, there is only one number (even if multiple entries with the same number).

--- documents/musterloesung-db-klausur-b/d3e.sql
+++ documents/musterloesung-db-klausur-b/d3e.sql
@@ -1,5 +1,4 @@
 SELECT berater_id, name, anzahl
     FROM Beratungsanzahl
     JOIN Berater ON Berater.berater_id = Beratungsanzahl.berater_id
-    WHERE anzahl = MAX(anzahl)
-    ORDER BY anzahl DESC
+    WHERE anzahl = (SELECT MAX(anzahl) FROM Beratungsanzahl)
