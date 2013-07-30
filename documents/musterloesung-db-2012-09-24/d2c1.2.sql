5a9336518749aeecf09e488167ad7264293709b7
Stefan Koch <taikano@lavabit.com>
2013-07-30 13:03:38 +0200

    Not an alternative, but introductory work
    This was not intended to be an alternative, but for explanation on how to begin such a query. This one only returns all required friendships including those already existing.
    
    Then better delete it completely.

--- documents/musterloesung-db-2012-09-24/d2c1.2.sql
+++
@@ -1,5 +0,0 @@
-SELECT f1.person2, f2.person2 
-    FROM FriendshipSymmetric f1
-    JOIN FriendshipSymmetric f2 ON f1.person1 = f2.person1
-    WHERE f1.person2 != f2.person2
-        AND f1.person1 = <id>;
