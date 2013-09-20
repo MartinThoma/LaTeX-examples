14b3575baffeb18fc81e06a75cc85633150bc85e
Martin Thoma <info@martin-thoma.de>
2013-09-20 18:24:17 +0200

    misc

--- documents/Numerik/Klausur6/aufgabe2.py
+++ documents/Numerik/Klausur6/aufgabe2.py
@@ -1,4 +1,4 @@
-from math import exp, log
+"""from math import exp, log
 
 def iterate(x):
     #return x - (2.0*x - exp(-x))/(2.0+exp(-x)) #Newton
@@ -9,3 +9,14 @@ x = 0.9
 for i in range(10):
     print (i, x)
     x = iterate(x)
+"""
+from math import log
+f = lambda x: -log(2*x)
+
+x = 0.35173371124919582602
+#x = 0.3517337112491958260249093009299510651715
+for i in range(200):
+    print("x 0.35173371124919582602")
+    print("x 0.3517337112491958260249093009299510651715")
+    print("%i %.30f" % (i, x))
+    x = f(x)
