9b9e6e6e004223d6bea3c84559a4fac5ba4081c3
Martin Thoma <info@martin-thoma.de>
2014-01-22 18:00:06 +0100

    Automatisch Liste von Definitionen erzeugen.

---
+++ documents/GeoTopo/definitions/generateDefinitions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import glob
+
+def get_definitions(filename):
+    with open(filename) as f:
+        content = f.read()
+
+    pattern = re.compile(r"\\begin{definition}.*?\\end{definition}", re.DOTALL)
+    m = re.findall(pattern, content)
+    return "\n\n".join(m)
+
+def write_definitions_to_template(definitions, template="mathe-vorlage.tex", target="definitionen.tex"):
+    with open(template) as f:
+        content = f.read()
+    content = content.replace('%CONTENT%', definitions)
+
+    with open(target, 'w') as f:
+        f.write(content)
+
+if __name__ == "__main__":
+    definitions = []
+    for texsource in sorted(glob.glob("../Kapitel*.tex")):
+        definitions.append(get_definitions(texsource))
+    write_definitions_to_template("\n\n\n".join(definitions))
+    
