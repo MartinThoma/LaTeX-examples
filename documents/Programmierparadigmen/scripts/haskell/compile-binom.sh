f93467bad7a38c1773382bac36bcc0f738afa9ce
Martin Thoma <info@martin-thoma.de>
2014-02-02 13:49:19 +0100

    SW

--- documents/Programmierparadigmen/scripts/haskell/compile-binom.sh
+++ documents/Programmierparadigmen/scripts/haskell/compile-binom.sh
@@ -1,9 +1,12 @@
 $ ghci binomialkoeffizient.hs 
-GHCi, version 7.4.2: http://www.haskell.org/ghc/  :? for help
+GHCi, version 7.4.2: 
+    http://www.haskell.org/ghc/  
+    :? for help
 Loading package ghc-prim ... linking ... done.
 Loading package integer-gmp ... linking ... done.
 Loading package base ... linking ... done.
-[1 of 1] Compiling Main             ( binomialkoeffizient.hs, interpreted )
+[1 of 1] Compiling Main             
+        ( binomialkoeffizient.hs, interpreted )
 Ok, modules loaded: Main.
 *Main> binom 5 2
 10
--- documents/Programmierparadigmen/scripts/haskell/fakultaet-akkumulator.hs
+++ documents/Programmierparadigmen/scripts/haskell/fakultaet-akkumulator.hs
@@ -1,2 +1,4 @@
-fakAcc n acc = if (n==0) then acc else fakAcc (n-1) (n*acc)
+fakAcc n acc = if (n==0) 
+               then acc 
+               else fakAcc (n-1) (n*acc)
 fak n = fakAcc n 1
