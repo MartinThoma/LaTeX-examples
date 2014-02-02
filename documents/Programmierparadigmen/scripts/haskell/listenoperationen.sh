dbf66188b617c7bf3eda129ee06f0417e1c20b66
Martin Thoma <info@martin-thoma.de>
2014-02-02 18:01:02 +0100

    Beispiele für Listenoperationen in Haskell hinzugefügt

---
+++ documents/Programmierparadigmen/scripts/haskell/listenoperationen.sh
@@ -0,0 +1,13 @@
+Prelude> let mylist = [1,2,3,4,5,6]
+Prelude> head mylist
+1
+Prelude> tail mylist
+[2,3,4,5,6]
+Prelude> take 3 mylist
+[1,2,3]
+Prelude> drop 2 mylist
+[3,4,5,6]
+Prelude> mylist
+[1,2,3,4,5,6]
+Prelude> mylist ++ sndList
+[1,2,3,4,5,6,9,8,7]
