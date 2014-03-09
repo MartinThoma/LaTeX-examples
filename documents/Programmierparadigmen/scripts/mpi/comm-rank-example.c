420b6b31fd0d91fe667551945f996ce428d3ee89
Martin Thoma <info@martin-thoma.de>
2014-03-09 21:01:47 +0100

    MPI

--- documents/Programmierparadigmen/scripts/mpi/comm-rank-example.c
+++ documents/Programmierparadigmen/scripts/mpi/comm-rank-example.c
@@ -1,13 +1,13 @@
 #include "mpi.h" 
 
-    int       rank;
-    MPI_Comm  comm;
+int       rank;
+MPI_Comm  comm;
 
-    ...
-    MPI_Comm_rank(comm, &rank);
-    if (rank==0) {
-        ... Code fur Prozess 0 ... 
-    }
-    else {
-        ... Code fur die anderen Prozesse ... 
-    }
+...
+MPI_Comm_rank(comm, &rank);
+if (rank==0) {
+    ... Code fur Prozess 0 ... 
+}
+else {
+    ... Code fur die anderen Prozesse ... 
+}
--- documents/Programmierparadigmen/scripts/mpi/mpi-reduce-example.c
+++ documents/Programmierparadigmen/scripts/mpi/mpi-reduce-example.c
@@ -1,10 +1,10 @@
 #include "mpi.h"
 
-    int  myid;
-    int  recvbuf[DATASIZE], sendbuf[DATA_SIZE];
+int  myid;
+int  recvbuf[DATASIZE], sendbuf[DATA_SIZE];
 
-    ...
-    /* Minimum bilden */
-    MPI_Reduce(sendbuf, recvbuf, DATA_SIZE, MPI_INT, MPI_MIN,
-           0, MPI_COMM_WORLD);
-    ...
+...
+/* Minimum bilden */
+MPI_Reduce(sendbuf, recvbuf, DATA_SIZE, MPI_INT, MPI_MIN,
+       0, MPI_COMM_WORLD);
+...
