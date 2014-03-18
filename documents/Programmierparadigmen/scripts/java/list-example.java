137a44cd5e976e43dc07c2124e32c159d3729ff9
Martin Thoma <info@martin-thoma.de>
2014-03-18 18:21:39 +0100

    Typisierung

---
+++ documents/Programmierparadigmen/scripts/java/list-example.java
@@ -0,0 +1,2 @@
+ArrayList<String>  l1 = new ArrayList<String>();
+ArrayList<Integer> l2 = new ArrayList<Integer>();
---
+++ documents/Programmierparadigmen/scripts/scala/duck-typing-example.scala
@@ -0,0 +1,4 @@
+def quacker(duck: 
+    {def quack(value: String): String}) {
+  println (duck.quack("like a duck!"))
+}
