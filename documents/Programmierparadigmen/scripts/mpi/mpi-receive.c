16d9ba2f2aaf95a82da42dfae0a002578d740eb8
Martin Thoma <info@martin-thoma.de>
2014-03-25 14:02:40 +0100

    MPI Datentypen hinzugefügt

--- documents/Programmierparadigmen/scripts/mpi/mpi-receive.c
+++ documents/Programmierparadigmen/scripts/mpi/mpi-receive.c
@@ -1,3 +1,4 @@
 int MPI_Recv(void *buf, int count, 
-             MPI_Datatype datatype, int source, int tag,
-             MPI_Comm comm, MPI_Status *status)
+             MPI_Datatype datatype, int source,
+             int tag, MPI_Comm comm, 
+             MPI_Status *status)
