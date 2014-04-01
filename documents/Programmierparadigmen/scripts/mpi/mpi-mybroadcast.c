fac84dbab33ac88320f37ae9d1b4362d8257c096
Martin Thoma <info@martin-thoma.de>
2014-04-01 14:44:24 +0200

    First- und Follow hinzugefügt

---
+++ documents/Programmierparadigmen/scripts/mpi/mpi-mybroadcast.c
@@ -0,0 +1,18 @@
+void my_bcast(void* data, int count, MPI_Datatype type,
+    int root, MPI_Comm comm) {
+    int my_rank;
+    MPI_Comm_rank(comm, &my_rank);
+    int comm_size;
+    MPI_Comm_size(comm, &comm_size);
+    if (my_rank == root) {
+        // If we are the root process, send our data to every one
+        for (int i = 0; i < comm_size; i++) {
+            if (i != my_rank) {
+                MPI_Send(data, count, type, i, 0, comm);
+            }
+        }
+    } else {
+        // If we are a receiver process, receive the data from root
+        MPI_Recv(data, count, type, root, 0, comm, MPI_STATUS_IGNORE);
+    }
+}
