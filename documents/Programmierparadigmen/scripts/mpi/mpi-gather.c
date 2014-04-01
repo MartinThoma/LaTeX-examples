a0962007a3d656f0b890d2c3c65a626876fe6ce0
Martin Thoma <info@martin-thoma.de>
2014-04-01 20:33:29 +0200

    Textsetzung

--- documents/Programmierparadigmen/scripts/mpi/mpi-gather.c
+++ documents/Programmierparadigmen/scripts/mpi/mpi-gather.c
@@ -1,3 +1,5 @@
-int MPI_Gather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
-    void *recvbuf, int recvcount, MPI_Datatype recvtype, int root,
-    MPI_Comm comm)
+int MPI_Gather(void *sendbuf, int sendcount, 
+    MPI_Datatype sendtype, 
+    void *recvbuf, int recvcount, 
+    MPI_Datatype recvtype, 
+    int root, MPI_Comm comm)
